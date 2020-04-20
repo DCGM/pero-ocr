de587f50c0ab30fa7514db2fc69449e9f4b2dedc
ikodym <oldrich.kodym@gmail.com>
2020-04-20 17:20:41 +0200

    cropper refactor and huge blending and reverse mapping speed up

--- pero_ocr/document_ocr/crop_engine.py
+++ pero_ocr/document_ocr/crop_engine.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
-from scipy import interpolate
+from scipy import interpolate, ndimage
 from numba import jit
-
 
 class EngineLineCropper(object):
 
@@ -13,22 +12,48 @@ class EngineLineCropper(object):
         self.scale = scale
         self.blend_border = blend_border
 
-    @jit
-    def reverse_value_mapping(self, forward_mapping, sample_positions, sampled_values):
-        backward_mapping = np.zeros_like(sample_positions)
-        forward_position = 0
-        for i in range(sample_positions.shape[0]):
-            while forward_mapping[forward_position] > sample_positions[i]:
-                forward_position += 1
-            d = forward_mapping[forward_position] - forward_mapping[forward_position-1]
-            da = (sample_positions[i] - forward_mapping[forward_position-1]) / d
-            backward_mapping[i] = (1 - da) * sampled_values[forward_position - 1] + da * sampled_values[forward_position]
-        return backward_mapping
+    def crop(self, img, baseline, heights, return_mapping=False):
+        try:
+            line_coords = self.get_crop_inputs(baseline, heights, self.line_height)
+
+            line_crop = cv2.remap(img, line_coords[:, :, 0], line_coords[:, :, 1],
+                                      interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+        except:
+            print("ERROR: line crop failed.", heights, baseline)
+            line_crop = np.zeros([self.line_height, 32, 3], dtype=np.uint8)
+
+        if return_mapping:
+            line_mapping, offset = self.reverse_xy_mapping(line_coords, img.shape)
+            return line_crop, line_mapping, offset
+        else:
+            return line_crop
+
+    def blend_in(self, img, line_crop, mapping, offset):
+        ystart = offset[0]
+        ystop = ystart + mapping.shape[0]
+        xstart = offset[1]
+        xstop = xstart + mapping.shape[1]
+
+        blended_img = img[ystart:ystop,xstart:xstop].copy()
+        mask = self.get_blend_mask(mapping)
+
+        cv2.remap(line_crop, mapping[:, :, 0], mapping[:, :, 1],
+            interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_TRANSPARENT,
+            dst=blended_img)
+
+        blended_img = np.round(
+            (1 - mask) * img[ystart:ystop,xstart:xstop] +
+            mask * blended_img
+            ).astype(np.uint8)
+
+        img[ystart:ystop,xstart:xstop] = blended_img
+
+        return img
 
     def get_crop_inputs(self, baseline, line_heights, target_height):
         line_heights = [line_heights[0], line_heights[1]]
-
         coords = np.asarray(baseline).copy().astype(int)
+        coords = np.stack((coords[:,1], coords[:,0]), axis=1) # OLD PERO_OCR MESSED COORDS, REMOVE WITH NEXT VERSION
         if self.poly:
             if coords.shape[0] > 2:
                 line_interpf = np.poly1d(np.polyfit(coords[:,0], coords[:,1], self.poly))
@@ -52,7 +77,7 @@ class EngineLineCropper(object):
         horizontal_sample_count = int(mapping_x_to_line_pos[-1] * scale) # number of target samples
 
         tmp = np.linspace(0, mapping_x_to_line_pos[-1], horizontal_sample_count)
-        output_x_positions = self.reverse_value_mapping( # get source x baseline positions in target pixels
+        output_x_positions = self.reverse_line_mapping( # get source x baseline positions in target pixels
             mapping_x_to_line_pos, tmp, line_x_values)
 
         output_y_positions = line_interpf(output_x_positions) # get source baseline y positions in target pixels
@@ -72,90 +97,74 @@ class EngineLineCropper(object):
 
         return coords
 
-    def crop(self, img, baseline, heights, return_mapping=False):
-        try:
-            line_coords = self.get_crop_inputs(baseline, heights, self.line_height)
-
-            line_crop = cv2.remap(img, line_coords[:, :, 0], line_coords[:, :, 1],
-                                      interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-        except:
-            print("ERROR: line crop failed.", heights, baseline)
-            line_crop = np.zeros([self.line_height, 32, 3], dtype=np.uint8)
-
-        if return_mapping:
-            line_mapping = self.reverse_mapping_fast(line_coords, img.shape)
-            return line_crop, line_mapping
-        else:
-            return line_crop
-
-    def blend_in(self, img, line_crop, mapping):
-
-        mapping = np.nan_to_num(mapping).astype(np.float32)
-
-        blended_img = img.copy()
-        cv2.remap(line_crop, mapping[:, :, 0], mapping[:, :, 1],
-            interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_TRANSPARENT,
-            dst=blended_img)
-        mask = (mapping[:,:,1] > -1)
-        mask = ndimage.morphology.binary_erosion(mask, iterations=self.blend_border).astype(np.float)
-        blur_kernel = np.ones((self.blend_border+1, self.blend_border+1)) / (self.blend_border+1) ** 2
-        mask = ndimage.convolve(mask, blur_kernel)
-        mask = mask[:, :, np.newaxis]
-        blended_img = (1 - mask) * img + mask * blended_img
-
-        return np.round(blended_img).astype(np.uint8)
-
-    def rigid_crop(self, img, baseline, height):
-        line_height = height[1] + height[0]
-        dy = 10
-        do = height[0] / 10
-        up = height[1] / 10
-
-        baseline = np.asarray(baseline)
-
-        one_line_points = []
-
-        p1 = baseline[0, :]
-        p2 = baseline[-1, :]
-        dir = (p2 - p1)
-        dir = dir / (dir ** 2).sum() ** 0.5
-        n = dir[::-1] * dy
-        n[0] = -n[0]
-        p1 = p1 - dir * dy * 0.5
-        p2 = p2 + dir * dy * 0.5
-
-        pts1 = np.asarray([p1 - do * n, p2 - do * n, p2 + up * n, p1 - up * n]).astype(np.float32)
-        one_line_points.append(np.copy(pts1))
-        pts1 = pts1[:3]
-
-        width = ((p2 - p1) ** 2).sum() ** 0.5 / (up + do) / dy * line_height
-        pts2 = np.asarray([(0, 0), (width, 0), (width, line_height)]).astype(np.float32)
-        pts1 = np.reshape(pts1, [-1, 1, 2])
-        pts2 = np.reshape(pts2, [-1, 1, 2])
-
-        T = cv2.getAffineTransform(pts1, pts2)
-        line_crop = cv2.warpAffine(img, T, (int(width + 0.5), int(line_height)))
-
-        return line_crop
+    @jit
+    def reverse_line_mapping(self, forward_mapping, sample_positions, sampled_values):
+        backward_mapping = np.zeros_like(sample_positions)
+        forward_position = 0
+        for i in range(sample_positions.shape[0]):
+            while forward_mapping[forward_position] > sample_positions[i]:
+                forward_position += 1
+            d = forward_mapping[forward_position] - forward_mapping[forward_position-1]
+            da = (sample_positions[i] - forward_mapping[forward_position-1]) / d
+            backward_mapping[i] = (1 - da) * sampled_values[forward_position - 1] + da * sampled_values[forward_position]
+        return backward_mapping
 
     @jit
-    def reverse_mapping_fast(self, forward_mapping, shape):
+    def reverse_xy_mapping(self, forward_mapping, shape):
+
         y_mapping = forward_mapping[:,:,1]
         y_mapping = np.clip(cv2.resize(y_mapping, (0,0), fx=4, fy=4, interpolation=cv2.INTER_LINEAR), 0, shape[1]-1)
         y_mapping = np.round(y_mapping).astype(np.int)
+        ystart = np.round(np.amin(y_mapping)).astype(np.int)
+        ystop = np.round(np.amax(y_mapping)).astype(np.int) + 1
+
         x_mapping = forward_mapping[:,:,0]
         x_mapping = np.clip(cv2.resize(x_mapping, (0,0), fx=4, fy=4, interpolation=cv2.INTER_LINEAR), 0, shape[0]-1)
         x_mapping = np.round(x_mapping).astype(np.int)
+        xstart = np.round(np.amin(x_mapping)).astype(np.int)
+        xstop = np.round(np.amax(x_mapping)).astype(np.int) + 1
 
         y_map = np.tile(np.arange(0, forward_mapping.shape[0]), (forward_mapping.shape[1], 1)).T.astype(np.float32)
         y_map = cv2.resize(y_map, (0,0), fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
         x_map = np.tile(np.arange(0, forward_mapping.shape[1]), (forward_mapping.shape[0], 1)).astype(np.float32)
         x_map = cv2.resize(x_map, (0,0), fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
 
-        reverse_mapping = np.ones((shape[0], shape[1], 2), dtype=np.float32) * -1
+        reverse_mapping = np.ones((ystop-ystart, xstop-xstart, 2), dtype=np.float32) * -1
 
         for sx, sy, dx, dy in zip(x_map.flatten(), y_map.flatten(), x_mapping.flatten(), y_mapping.flatten()):
-            reverse_mapping[dy, dx, 0] = sx
-            reverse_mapping[dy, dx, 1] = sy
+            # print(dy, ystart, dx, xstart)
+            reverse_mapping[dy-ystart, dx-xstart, 0] = sx
+            reverse_mapping[dy-ystart, dx-xstart, 1] = sy
 
-        return reverse_mapping
+        return reverse_mapping, (ystart, xstart)
+
+    def get_blend_mask(self, mapping):
+        mask = mapping[:,:,0] > -1
+        mask = np.pad(mask, ((self.blend_border,self.blend_border), (self.blend_border,self.blend_border)))
+        mask = ndimage.uniform_filter(mask.astype(np.float), size=2*self.blend_border+1)
+        mask = mask[self.blend_border:-self.blend_border, self.blend_border:-self.blend_border]
+        mask = 2 * np.clip(mask-0.5, 0, 1)
+        return mask[:, :, np.newaxis]
+
+def main():
+
+    from pero_ocr.document_ocr import layout
+    import matplotlib.pyplot as plt
+
+    page_img = cv2.imread('../../../example/82f4ac84-6f1e-43ba-b1d5-e2b28d69508d.jpg')
+    page_layout = layout.PageLayout(file='../../../example/82f4ac84-6f1e-43ba-b1d5-e2b28d69508d.xml')
+
+    cropper = EngineLineCropper(line_height=48, poly=2, scale=1)
+    cropped_line, mapping = cropper.crop(page_img, page_layout.regions[2].lines[0].baseline, page_layout.regions[2].lines[0].heights, return_mapping=True)
+    back_mapped = cropper.blend_in(page_img, cropped_line, mapping)
+
+    plt.subplot(131)
+    plt.imshow(cropped_line)
+    plt.subplot(132)
+    plt.imshow(back_mapped[410:420, 1200:1240])
+    plt.subplot(133)
+    plt.imshow(np.concatenate((mapping, mapping[:,:,:1]), axis=2).astype(np.int)[410:420, 1200:1240])
+    plt.show()
+
+if __name__ == '__main__':
+    main()
