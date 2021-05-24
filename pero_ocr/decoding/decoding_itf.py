aee30255dd1ed398e9f23e9d24c275cfbd6ec6d4
Benes Karel <ibenes@fit.vutbr.cz>
2021-05-24 20:50:44 +0200

    Implement support for TorchScript LMs

--- pero_ocr/decoding/decoding_itf.py
+++ pero_ocr/decoding/decoding_itf.py
@@ -5,6 +5,8 @@ import sys
 import json
 from .decoders import GreedyDecoder, CTCPrefixLogRawNumpyDecoder, BLANK_SYMBOL
 from pero_ocr.utils import compose_path
+from brnolm.language_models import language_model
+import logging
 
 ZERO_LOGITS = -80.0
 
@@ -16,8 +18,15 @@ def get_ocr_charset(fn):
     return chars
 
 
-def construct_lm(path, config_path=''):
-    lm = torch.load(compose_path(path, config_path), map_location=torch.device('cpu'))
+def construct_lm(path, config_path='', logger=logging):
+    try:
+        lm = language_model.torchscript_import(compose_path(path, config_path))
+    except language_model.UnreadableModelError as e:
+        logger.warning('WARNING: Failed to load model as TorchScript file, original error:\n')
+        logger.warning(f'{e}\n')
+        logger.warning('WARNING: Attempting to load as a plain torch-pickled model...\n')
+        lm = torch.load(compose_path(path, config_path), map_location=torch.device('cpu'))
+
     lm._unused_prefix_len = 2
 
     return lm
