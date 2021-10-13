e6344ebc879ff966b0a429fd06f718c39facb237
Benes Karel <ibenes@fit.vutbr.cz>
2021-10-13 15:29:02 +0200

    Improve PEP8 compliancy

--- pero_ocr/utils.py
+++ pero_ocr/utils.py
@@ -2,7 +2,6 @@ from os.path import isabs, join
 import os
 import sys
 import subprocess
-import re
 
 try:
     subprocess.check_output(
@@ -10,17 +9,20 @@ try:
     )
     print('numba available, importing jit')
     from numba import jit
-except:
+except Exception:
     print('cannot import numba, creating dummy jit definition')
+
     def jit(function):
-        def wrapper(*args,**kwargs):
-            return function(*args,**kwargs)
+        def wrapper(*args, **kwargs):
+            return function(*args, **kwargs)
         return wrapper
+
 
 def compose_path(file_path, reference_path):
     if reference_path and not isabs(file_path):
         file_path = join(reference_path, file_path)
     return file_path
+
 
 def setGPU():
     freeGpu = subprocess.check_output('nvidia-smi -q | grep "Minor\|Processes" | grep "None" -B1 | tr -d " " | cut -d ":" -f2 | sed -n "1p"', shell=True)
