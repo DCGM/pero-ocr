3558e7526f5eb4cff2a6923e10f84c69bf8a28f8
Benes Karel <ibenes@fit.vutbr.cz>
2021-10-14 13:00:32 +0200

    Remove the now unused code for GPU acquisition

--- pero_ocr/utils.py
+++ pero_ocr/utils.py
@@ -1,5 +1,4 @@
 from os.path import isabs, join
-import os
 import sys
 import subprocess
 
@@ -22,16 +21,3 @@ def compose_path(file_path, reference_path):
     if reference_path and not isabs(file_path):
         file_path = join(reference_path, file_path)
     return file_path
-
-
-def setGPU():
-    freeGpu = subprocess.check_output('nvidia-smi -q | grep "Minor\|Processes" | grep "None" -B1 | tr -d " " | cut -d ":" -f2 | sed -n "1p"', shell=True)
-    freeGpu = freeGpu.decode().strip()
-    if len(freeGpu) == 0:
-        print('no free GPU')
-        sys.exit(1)
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = freeGpu
-    print('got GPU ' + (freeGpu))
-
-    return(int(freeGpu.strip()))
