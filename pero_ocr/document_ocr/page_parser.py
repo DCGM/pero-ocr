11eb20ea1e9653abdd7fc1c92b856cef782c9edc
Benes Karel <ibenes@fit.vutbr.cz>
2024-02-21 15:15:04 +0100

    Prevent attempts to operate on transformer logits as if they were CTC

--- pero_ocr/document_ocr/page_parser.py
+++ pero_ocr/document_ocr/page_parser.py
@@ -403,7 +403,7 @@ class LineCropper(object):
                 print(f"WARNING: Failed to crop line {line.id}. Probably contain vertical line. Contanct Olda Kodym to fix this bug!")
 
 
-class PageOCR(object):
+class PageOCR:
     def __init__(self, config, device, config_path=''):
         json_file = compose_path(config['OCR_JSON'], config_path)
         use_cpu = config.getboolean('USE_CPU')
@@ -429,6 +429,10 @@ class PageOCR(object):
             line.logit_coords = line_logit_coords
         return page_layout
 
+    @property
+    def provides_ctc_logits(self):
+        return isinstance(self.ocr_engine, PytorchEngineLineOCR)
+
 
 def get_prob(best_ids, best_probs):
     last_id = -1
@@ -447,7 +451,7 @@ def get_prob(best_ids, best_probs):
 
 
 def get_default_device():
-    return torch.device('cuda') if torch.cuda.is_available() else torch.device ('cpu')
+    return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 
 class PageParser(object):
@@ -490,6 +494,13 @@ class PageParser(object):
         #     print(best_probs[i-1:i+2], best_ids[i-1:i+2])
 
         return worst_best_prob
+
+    @property
+    def provides_ctc_logits(self):
+        if not self.ocr:
+            return False
+
+        return self.ocr.provides_ctc_logits
 
     def update_confidences(self, page_layout):
         for line in page_layout.lines_iterator():
